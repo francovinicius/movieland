58a5946222645bc2d8928ed7ff5f35803679ad32
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-07-18 13:48:06 -0300

    testII

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import SearchIcon from './search.svg'
 import MovieCard from './MovieCard';
 
-const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7';
+const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7&';
 
 const movie1 =
 {

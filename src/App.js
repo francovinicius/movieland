f6ce7935caafd7769b9df2f1f08cca1e3c4494d8
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-07-18 13:23:20 -0300

    Criando component card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import './App.css';
 
 import SearchIcon from './search.svg'
+import MovieCard from './MovieCard';
 
 const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7';
 
@@ -49,25 +50,7 @@ function App() {
       </div>
 
       <div className='container'>
-        <div className='movie'>
-
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-
-          <div>
-            <img
-              src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placheholder.com/400'}
-              alt={movie1.Title}
-            />
-          </div>
-
-          <div>
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
-          </div>
-
-        </div>
+        <MovieCard  movie1={movie1}/>
       </div>
     </div>
   );

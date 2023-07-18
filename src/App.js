df67c562538b9c2fe72e0d46e2922734a1663e30
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-07-18 13:14:33 -0300

    add css do projeto / Titulo / input / card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import { useEffect  } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
-const API_URL ='http://www.omdbapi.com?apikey=c032e2d7';
+import SearchIcon from './search.svg'
+
+const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7';
+
+const movie1 =
+{
+  "Title": "Miles Morales Ultimate Spiderman",
+  "Year": "2021",
+  "imdbID": "tt14311386",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BNmMzODkwNDktMTkyMy00MmU5LWE4MGMtYzIzZjdjNmJiZDRiXkEyXkFqcGdeQXVyNDU1NDQ0NzE@._V1_SX300.jpg"
+}
+
 
 function App() {
 
 
-    const searchMovies = async (tittle) => {
-      const response = await fetch(`${API_URL}&S=${tittle}`);
-      const data = await response.json();
+  const searchMovies = async (tittle) => {
+    const response = await fetch(`${API_URL}&S=${tittle}`);
+    const data = await response.json();
 
-      console.log(data.Search)
-    }
-  
-    useEffect(() => {
-      searchMovies('Spiderman');
-    }, []);
+    console.log(data.Search)
+  }
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
 
   return (
-    <div className="App">
-      <h1>Teste</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className='search'>
+
+        <input
+          placeholder='Search for Movies'
+          value='Superman'
+          onChange={() => { }}
+        />
+        <img
+          src={SearchIcon}
+          alt='search'
+          onClick={() => { }}
+        />
+
+      </div>
+
+      <div className='container'>
+        <div className='movie'>
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+          <img
+          src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placheholder.com/400'}
+          alt={movie1.Title}
+
+        />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

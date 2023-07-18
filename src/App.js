eb5735c5a9f2bc244e5661f48a0f5be07c2fb2ea
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-07-18 13:17:12 -0300

    Finalizado card movies

--- src/App.js
+++ src/App.js
@@ -50,16 +50,23 @@ function App() {
 
       <div className='container'>
         <div className='movie'>
+
           <div>
             <p>{movie1.Year}</p>
           </div>
-          <div>
-          <img
-          src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placheholder.com/400'}
-          alt={movie1.Title}
 
-        />
+          <div>
+            <img
+              src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placheholder.com/400'}
+              alt={movie1.Title}
+            />
           </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+
         </div>
       </div>
     </div>
